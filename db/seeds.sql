346a024cc4d7e517bc65ff278f8f8efa3154226f
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-07-10 15:41:16 -0700

    created file structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Ollie's Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('BLT');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeese Burger');
+INSERT INTO burgers (burger_name) VALUES ("Vegi-Burger");
+INSERT INTO burgers (burger_name) VALUES ('Fake-burger burger');
