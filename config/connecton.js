0a3d356e705c278791f2678c54a38801521763e0
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-07-10 18:01:03 -0700

    wip

--- config/connecton.js
+++ config/connecton.js
@@ -1,13 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "cat_db"
-});
+
+var connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.comt',
+    user: 'koed0d17foxpgcix',
+    password: 'bzujtayvuzgqt6bk',
+    database: 'vy3jxqnv9xzs8da4'
+  })
+};
+  
+
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "cat_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
