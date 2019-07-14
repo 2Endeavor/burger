5acb90abb866d3176adb4222d6215f70ea8795fb
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-07-13 17:19:16 -0700

    logic is working to place items in correct columns

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 USE burgers_db;
-INSERT INTO burgers (burger_name) VALUES ("Ollie's Cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
-INSERT INTO burgers (burger_name) VALUES ('BLT');
-INSERT INTO burgers (burger_name) VALUES ('Double Cheeese Burger');
-INSERT INTO burgers (burger_name) VALUES ("Vegi-Burger");
-INSERT INTO burgers (burger_name) VALUES ('Fake-burger burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ollie's Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegi-Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fake-burger burger',true);
