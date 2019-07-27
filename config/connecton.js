c45fbb346e9785c641c1279fcf35815adaa113dc
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-07-27 10:21:36 -0700

    updated database connection

--- config/connecton.js
+++ config/connecton.js
@@ -7,11 +7,16 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
     // host: 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    host: 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'koed0d17foxpgcix',
-    password: 'bzujtayvuzgqt6bk',
-    database: 'vy3jxqnv9xzs8da4'
+    // host: 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    // user: 'koed0d17foxpgcix',
+    // password: 'bzujtayvuzgqt6bk',
+    // database: 'vy3jxqnv9xzs8da4'
   })
 };
   
