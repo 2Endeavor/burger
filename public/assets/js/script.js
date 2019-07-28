4229ff0a0c9fecfb4a16259caded156e57a99897
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-07-28 14:33:00 -0700

    corrected error

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
-  $(".change-devoured").on("click", function(event) {
-    var id = $(this).data("id");
-    console.log(id);
-    var newDevoured = $(this).data("newdevoured");
-    console.log(newDevoured);
+$(".change-devoured").on("click", function(event) {
 
-    var newDevouredState = {
-      devoured: newDevoured
-    };
-  });
+
+  var id = $(this).data("id");
+  console.log(id);
+  var newDevoured = $(this).data("newdevoured");
+  console.log(newDevoured);
+
+  var newDevouredState = {
+    devoured: newDevoured
+  
+  };
+
 
   // Send the PUT request.
   $.ajax("/api/burgers/" + id, {
     type: "PUT",
     data: newDevouredState
-  }).then(function() {
-    console.log("changed devoured to", newDevoured);
-    // Reload the page to get the updated list
-    location.reload();
-  });
+  }).then(
+    function() {
+      console.log("changed devoured to", newDevoured);
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
 });
+})
